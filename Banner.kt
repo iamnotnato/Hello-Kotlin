ab5c3f1325764a87c8738dd97b83ea8b8139c349
Alan Nato <alanwnato@gmail.com>
2022-12-06 23:58:55 +0300

    Functions

--- Banner.kt
+++ Banner.kt
@@ -1,13 +1,21 @@
 fun main() {
-    val border = "%"
+    val border = "<"
+    val lowerborder = ">"
     printBorder(border)
     println("Happy Birthday, Jhansi!")
-    printBorder(border)
+    printBorder(lowerborder)
 }
 
 fun printBorder(border: String) {
     repeat(23) {
         print(border)
+    }
+    println()
+}
+
+fun printLowerBorder(lowerborder: String) {
+    repeat(23) {
+        print(lowerborder)
     }
     println()
 }
