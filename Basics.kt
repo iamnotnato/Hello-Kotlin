55d7352100f881085f890766664bc29fe45641fc
Alan Nato <alanwnato@gmail.com>
2022-12-06 23:46:03 +0300

    First

---
+++ Basics.kt
@@ -0,0 +1,25 @@
+//A function is a section of a program that performs a specific task.
+//This function is called main, because it is the first, or main, function that is called when you run the program. 
+//Every Kotlin program needs a function named main.
+//The function name is always followed by () two parentheses.
+//Inside the parentheses, you can put information for the function to use. This input to the function is called "arguments" or args for short. You will learn more about arguments later.
+//Notice the pair of curly braces {} after the parentheses. Inside a function is code that accomplishes a task. These curly braces surround those lines of code.
+//println tells the system to print a line of text.
+//Inside the parentheses you put the text to be printed.
+//Notice that the text to be printed is surrounded by quotes. This tells the system that everything inside the quotation marks should be printed exactly as given
+fun main() {
+    println("Happy Birthday!")
+    println("You are 34!")
+    println("with line \n break")
+
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+
+    // This prints an empty line.
+    println("")
+}
