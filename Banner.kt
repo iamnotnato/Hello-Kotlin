46279a76db50ce0d2ea400810b682261ff70d9e0
Alan Nato <alanwnato@gmail.com>
2022-12-06 23:51:02 +0300

    Print Border

---
+++ Banner.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("=======================")
+    println("Happy Birthday, Jhansi!")
+    println("=======================")
+}
+
+fun printBorder() {
+    println("=======================")
